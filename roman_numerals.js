d2729c9a8e8eae79684721b1932991cd71528c81
Windiana <windi@hacktiv8.com>
2016-09-23 15:06:16 +0700

    Template

---
+++ roman_numerals.js
@@ -0,0 +1,26 @@
+function to_roman_old(n) {
+  // your code here
+}
+
+function to_roman(n) {
+  // your implementation code here
+}
+
+// Drive code
+console.log("My totally sweet testing script\n");
+console.log("input | expected | actual");
+console.log("———|—————|———");
+console.log("4     | IIII     | ", to_roman_old(4));
+console.log("9     | VIIII    | ", to_roman_old(9));
+console.log("13    | XIII     | ", to_roman_old(13));
+console.log("1453  | MCDLIII  | ", to_roman_old(1453));
+console.log("1646  | MDCXLVI  | ", to_roman_old(1646));
+
+console.log("My totally sweet testing script for new roman\n");
+console.log("input | expected | actual");
+console.log("———|—————|———");
+console.log("4     | IV       | ", to_roman(4));
+console.log("9     | IX       | ", to_roman(9));
+console.log("13    | XIII     | ", to_roman(13));
+console.log("1453  | MCDLIII  | ", to_roman(1453));
+console.log("1646  | MDCXLVI  | ", to_roman(1646));
