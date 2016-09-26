b26f2bde8b59312a8efa19894929f75c146a2a26
Alessandro C & Juang Wiantoro <pair+alessandrocls.broerjuang@hacktiv8.com>
2016-09-26 15:42:46 +0700

    Final Submission roman numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,9 +1,30 @@
-function to_roman_old(n) {
-  // your code here
+function to_roman_old(num) {
+  // Reference to old roman
+  var roman = ["M","CM", "D", "CD", "C","XC", "L", "XL", "X", "V", "I"],
+  arabic = [1000,900, 500, 400, 100, 90, 50, 40, 10, 5, 1], result = [];
+  // looping through the arabic and check if the num is met the while condition
+  for (var i = 0; i < arabic.length; i += 1) {
+    while (num % arabic[i] < num) {
+      result.push(roman[i]);
+      // update num by the condition that it meets
+      num -= arabic[i];
+    }
+  }
+  return result.join("");
 }
 
-function to_roman(n) {
-  // your implementation code here
+function to_roman(num) {
+  // update new roman number
+  var roman = ["M","CM", "D", "CD", "C","XC", "L", "XL", "X","IX", "V", "IV", "I"],
+  arabic = [1000,900, 500, 400, 100, 90, 50, 40, 10, 9, 5,4, 1], result = [];
+
+  for (var i = 0; i < arabic.length; i += 1) {
+    while (num % arabic[i] < num) {
+      result.push(roman[i]);
+      num -= arabic[i];
+    }
+  }
+  return result.join("");
 }
 
 // Drive code
